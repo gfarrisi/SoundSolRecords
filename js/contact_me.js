04f8516ab259a264858761ed96708524ad9dbde9
gfarrisi <gabriellafarrisi@gmail.com>
2020-03-15 16:01:19 -0400

    added form post

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,6 +46,7 @@
 //           $('#success').html("<div class='alert alert-danger'>");
 //           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
 //             .append("</button>");
+// console.log("I am still getting clicked");
 //           $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
 //           $('#success > .alert-danger').append('</div>');
 //           //clear all fields
